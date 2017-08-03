84ee87bdf20eaa6462ef1f4d7dde8a6373a36300
RobelsiTejada <robelsitejada@gmail.com>
2017-08-03 02:23:55 -0400

    3rdcycle 2nd commit
    -changed the entire js due to complexity of my previous set algorithms

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,12 +8,9 @@ $(() => {
 })
 
 // use require without a reference to ensure a file is bundled
-// require('./main.js')
-require('./clicky.js')
-require('./getapi.js')
-require('./postapi.js')
-require('./patchapi.js')
-require('./deleteapi.js')
-// require('./user.js')
-// require('./gamelogic.js')
-// require('./results.js')
+// require('./main')
+// require('./getapi')
+// require('./postapi')
+// require('./patchapi')
+// require('./deleteapi')
+require('./gamelogic')
