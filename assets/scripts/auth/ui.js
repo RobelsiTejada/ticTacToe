f71113603c60fa843ef2e6701728b90102666099
RobelsiTejada <robelsitejada@gmail.com>
2017-09-25 23:12:25 -0400

    testing fix sign out

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,47 +1,67 @@
 'use strict'
 
+const app = require('../app.js')
+
+const signInSuccess = (data) => {
+  app.user = data.user
+  $('#authenticate').hide()
+  $('#newpassword').hide()
+  $('#login').hide()
+  $('#checkout').show()
+  $('#logedin').show()
+  $('#passwordChange').show()
+}
+
+const signOutSuccess = () => {
+  app.user = null
+  $('#newpassword').hide()
+  $('#checkout').hide()
+  $('#passwordChange').hide()
+  $('#newuser2').hide()
+  $('#logedin').hide()
+  $('#authenticate').show()
+  $('#newuser').show()
+  $('#login').show()
+}
+
+const changePasswordSuccess = () => {
+  $('#newpassword').hide()
+}
+
 const signUpSuccess = (data) => {
-  console.log(data)
-  $('table').show()
-  $('.modal').modal('hide')
-  $('.messages').html('')
-  $('.turn').html('Ninja 1\'s turn')
+  $('#newusererrorp').hide()
+  $('#newuser').hide()
+  $('#newpassword').hide()
+  $('#passwordChange').hide()
+  $('#newuser2').show()
 }
 
 const signUpFailure = (error) => {
   console.error(error)
-}
-
-const signInSuccess = (data) => {
-  console.log(data)
-  $('table').show()
-  $('.modal').modal('hide')
-  $('.messages').html('')
-  $('.turn').html('Ninja 1\'s turn')
+  $('#newpassword').hide()
+  $('#passwordChange').hide()
 }
 
 const signInFailure = (error) => {
   console.error(error)
+  $('#passwordChange').hide()
+  $('#loginerrorp').show()
 }
 
-const updateSuccess = function () {
-  console.log('You successfully updated your password!')
+const success = (data) => {
 }
 
-const signOutSuccess = function () {
-  console.log('You have successfully signed out.')
-}
-
-const onError = function (response) {
-  console.error(response)
+const failure = (error) => {
+  console.error(error)
 }
 
 module.exports = {
-  updateSuccess,
-  signInSuccess,
-  signInFailure,
-  signUpSuccess,
+  failure,
+  success,
   signUpFailure,
+  signInFailure,
+  signInSuccess,
   signOutSuccess,
-  onError
+  changePasswordSuccess,
+  signUpSuccess
 }
